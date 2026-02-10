3242591a78c49a7383d5e845f942571f2490718f
jugaseme <sebastiangarciamedina26@gmail.com>
2026-02-10 09:58:39 -0500

    espacio_principal

--- tienda_1/src/App.jsx
+++ tienda_1/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import products from './data/productos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Cart, setCart] = useState([])
+
+  const addToCart = (product) => {
+    setCart(prevCart => [...prevCart, product])
+  }
+
+   const total = Cart.reduce((acc, item) => acc + item.price, 0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Catalogo De Tienda</h1>
+
+        <ul>
+          {products.map(products => (
+            <li key={products.id}>
+              <strong>{products.name}</strong> - ${products.price.toLocaleString("es-CO")} 
+              <button onClick={()=> addToCart(products)}>Agregar al carrito</button>
+            </li>
+          ))}
+        </ul>
+            <h2>Total en el Carrito: ${total.toLocaleString("es-CO")}</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ tienda_1/src/data/productos.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "Camiseta básica", price: 45000, category: "Ropa" },
+  { id: 2, name: "Gorra urbana", price: 30000, category: "Accesorios" },
+  { id: 3, name: "Pantalón cargo", price: 95000, category: "Ropa" },
+]
+
+export default products
